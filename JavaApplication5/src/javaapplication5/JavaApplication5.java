f7129e6e39ae88562569165aba282b103a69e910
Santy <Santy@LAPTOP-9UVNOQ5S.fibertel.com.ar>
2021-05-26 15:10:09 -0300

    Tercera actualizacion

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -1,14 +1,11 @@
-
 package javaapplication5;
 
 /**
- * @author Santy
+ * @author Santiago Reynoso
  */
 public class JavaApplication5 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
         System.out.println("Hola Git");
     }
-    
 }
