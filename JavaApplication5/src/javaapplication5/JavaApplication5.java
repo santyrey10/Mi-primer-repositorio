cef2cfc0ff77ad1eb799a470e3ae8fa6d503710d
Santy <Santy@LAPTOP-9UVNOQ5S.fibertel.com.ar>
2021-05-26 15:01:52 -0300

    Primer actualizacion

---
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,13 @@
+
+package javaapplication5;
+
+/**
+ * @author Santy
+ */
+public class JavaApplication5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Git");
+    }
+    
+}
